142624f70e4efa83dcf8f0c2b98ec5a86361dc87
Mikhail Chernykh <mikhail@rhodecode.com>
2015-06-03 17:41:47 +0200

    Improve vcs selection workflow

--- flake8diff/exceptions.py
+++ flake8diff/exceptions.py
@@ -1,7 +1,15 @@
 from __future__ import unicode_literals, print_function
 
+import six
 
-class Flake8NotInstalledError(Exception):
+
+@six.python_2_unicode_compatible
+class BaseError(Exception):
+    def __str__(self):
+        return self.message
+
+
+class Flake8NotInstalledError(BaseError):
     """
     Exception when run_flake8 installation cannot be found.
     """
@@ -9,14 +17,14 @@ class Flake8NotInstalledError(Exception):
                'Is it on $PATH?')
 
 
-class NotLocatableVCSError(Exception):
+class NotLocatableVCSError(BaseError):
     """
     Exceptions for when VCS cannot be determined automatically
     """
     message = 'VCS could not be determined automatically'
 
 
-class UnsupportedVCSError(Exception):
+class UnsupportedVCSError(BaseError):
     """
     Exception for handling unknown VCS
     """
@@ -24,9 +32,10 @@ class UnsupportedVCSError(Exception):
     def __init__(self, vcs=None):
         msg = '{0} VCS is not unsupported'
         self.message = msg.format(vcs)
+        super(UnsupportedVCSError, self).__init__()
 
 
-class VCSNotInstalledError(Exception):
+class VCSNotInstalledError(BaseError):
     """
     Exception for when particular vcs installation
     cannot be found.
@@ -36,3 +45,15 @@ class VCSNotInstalledError(Exception):
         msg = ('VCS "{0}" installation could not be found. '
                'Is it on $PATH?')
         self.message = msg.format(vcs)
+        super(VCSNotInstalledError, self).__init__()
+
+
+class WrongVCSSpecified(BaseError):
+    """
+    Exception for when particular vcs installation
+    cannot be found.
+    """
+
+    def __init__(self, vcs=None):
+        self.message = 'This is not a "{0}" repository'.format(vcs or '')
+        super(WrongVCSSpecified, self).__init__()
