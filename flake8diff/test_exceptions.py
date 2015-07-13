3e22f372a65b7691ab0cc2a5d0ad17a9ba61b6b8
Gregory Armer <gregory.armer@dealertrack.com>
2015-07-13 15:57:44 -0400

    assertEquals() -> assertEqual() since assertEquals() is deprecated

--- flake8diff/test_exceptions.py
+++ flake8diff/test_exceptions.py
@@ -18,7 +18,7 @@ class ExceptionsTestCase(TestCase):
         try:
             raise Flake8NotInstalledError
         except Flake8NotInstalledError as e:
-            self.assertEquals(str(e), expected)
+            self.assertEqual(str(e), expected)
 
     def test_not_locatable_vcs(self):
         expected = "VCS could not be determined automatically"
@@ -26,7 +26,7 @@ class ExceptionsTestCase(TestCase):
         try:
             raise NotLocatableVCSError
         except NotLocatableVCSError as e:
-            self.assertEquals(str(e), expected)
+            self.assertEqual(str(e), expected)
 
     def test_unsupported_vcs(self):
         expected = "foo VCS is not supported"
@@ -34,7 +34,7 @@ class ExceptionsTestCase(TestCase):
         try:
             raise UnsupportedVCSError(vcs="foo")
         except UnsupportedVCSError as e:
-            self.assertEquals(str(e), expected)
+            self.assertEqual(str(e), expected)
 
     def test_vcs_not_installed(self):
         expected = 'VCS "bar" installation could not be found. Is it on $PATH?'
@@ -42,7 +42,7 @@ class ExceptionsTestCase(TestCase):
         try:
             raise VCSNotInstalledError(vcs="bar")
         except VCSNotInstalledError as e:
-            self.assertEquals(str(e), expected)
+            self.assertEqual(str(e), expected)
 
     def test_wrong_vcs_specified(self):
         expected = 'This is not a "baz" repository'
@@ -50,4 +50,4 @@ class ExceptionsTestCase(TestCase):
         try:
             raise WrongVCSSpecified(vcs="baz")
         except WrongVCSSpecified as e:
-            self.assertEquals(str(e), expected)
+            self.assertEqual(str(e), expected)
