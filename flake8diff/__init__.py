bec04390ce9263fc7957ccb7fcf39d582fa8d4ad
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2015-08-03 12:21:21 -0400

    bumped version to 0.1.2 which addresses #29

--- flake8diff/__init__.py
+++ flake8diff/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __description__ = (
     'Run flake8 across a set of changed files and filter out '
     'violations occurring only on the lines that were changed.'
